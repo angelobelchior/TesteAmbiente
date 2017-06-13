be2e7e17aa0ab734eb9a5281e5819ca2a6cd3c40
Angelo Luis <angelobelchior@hotmail.com>
2017-06-13 14:07:51 -0300

    atualização dos pacotes

--- TesteAmbiente/TesteAmbiente/Properties/AssemblyInfo.cs
+++ TesteAmbiente/TesteAmbiente/Properties/AssemblyInfo.cs
@@ -2,19 +2,21 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xamarin.Forms.Xaml;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TesteAmbiente")]
+[assembly: AssemblyTitle("Teste de Ambiente")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TesteAmbiente")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 // Version information for an assembly consists of the following four values:
 //
