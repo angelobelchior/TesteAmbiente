bba1985e46c5ed58dfb2232df9daf8364fe6531d
Angelo Luis <angelobelchior@hotmail.com>
2017-06-13 13:57:05 -0300

    Projeto Final

---
+++ TesteAmbiente/TesteAmbiente/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TesteAmbiente
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            btnEntrar.Clicked += BtnEntrar_Clicked;
+        }
+
+        protected override void OnDisappearing()
+        {
+            btnEntrar.Clicked -= BtnEntrar_Clicked;
+        }
+
+        private void BtnEntrar_Clicked(object sender, EventArgs e)
+        {
+            var masterDetail = new MasterDetailPage
+            {
+                Title = "Teste de Ambiente",
+                Master = new Master(),
+                Detail = new NavigationPage(new Detail())
+            };
+
+            App.Current.MainPage = masterDetail;
+        }
+    }
+}
